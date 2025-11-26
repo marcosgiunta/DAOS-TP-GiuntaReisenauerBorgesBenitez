07864b7c02bf618d34fee60fd24e039e984c2070
Dayse Borges <dayseborges030798@gmail.com>
2025-11-25 23:37:43 -0300

    Endpoints para Asistencias

--- src/main/java/tuti/daos/entidades/EntregaAsistencia.java
+++ src/main/java/tuti/daos/entidades/EntregaAsistencia.java
@@ -1,7 +1,12 @@
 package tuti.daos.entidades;
-import jakarta.persistence.*;
+import java.util.Date;
 
-import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
 
 
 @Entity
@@ -10,41 +15,67 @@ public class EntregaAsistencia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private LocalDate fecha;
-    private Integer cantidadRaciones;
 
-    @ManyToOne
-    @JoinColumn(name = "preparacion_id")
-    private Preparacion preparacion;
+    private Integer idAsistido;
+    private Integer idRacionEntregada;
+    private Date fechaEntrega;
 
+    // @ManyToOne
+    // @JoinColumn(name = "preparacion_id")
+    // private Preparacion preparacion;
 
-    public Preparacion getPreparacion() {
-        return preparacion;
-    }
-    public void setPreparacion(Preparacion preparacion) {
-        this.preparacion = preparacion;
-    }
-  
+    // @ManyToOne
+    // @JoinColumn(name = "familia_id")
+    // private Familia familia;
+
+    //voluntario id en la base de datos aparece como voluntario_id = null 
+    //esto sucede porque para el TP no se pidio en las historias de usuario
+    //que se guarde el voluntario que realiza la entrega, por lo tanto no se guarda
+    //pero se deja el campo para que en un futuro se pueda implementar 
+    // @ManyToOne
+    // @JoinColumn(name = "voluntario_id")
+    // private Voluntario voluntario;
+
+    // public Preparacion getPreparacion() {
+    //     return preparacion;
+    // }
+    // public void setPreparacion(Preparacion preparacion) {
+    //     this.preparacion = preparacion;
+    // }
+    // public Familia getFamilia() {
+    //     return familia;
+    // }
+
+    // public void setFamilia(Familia familia) {
+    //     this.familia = familia;
+    // }
 
     public Integer getId() {
         return id;
-    }   
-    public void setId(Integer id) {
-        this.id = id;
     }
-    public LocalDate getFecha() {
-        return fecha;
+
+    public Integer getIdAsistido() {
+        return idAsistido;
     }
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
+
+    public void setIdAsistido(Integer idAsistido) {
+        this.idAsistido = idAsistido;
     }
-    public Integer getCantidadRaciones() {
-        return cantidadRaciones;
+
+    public Integer getIdRacionEntregada() {
+        return idRacionEntregada;
     }
-    public void setCantidadRaciones(Integer cantidadRaciones) {
-        this.cantidadRaciones = cantidadRaciones;
+
+    public void setIdRacionEntregada(Integer idRacionEntregada) {
+        this.idRacionEntregada = idRacionEntregada;
+    }
+
+    public Date getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(Date fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
     }
  
-
-    
 }
